3e252c7c3babfefc686cd544747baf0da4cff7bc
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-11-10 19:43:44 +0000

    added a new cell

---
+++ splittable_tt/splittable_tt/TableViewCellBanner.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCellBanner.swift
+//  splittable_tt
+//
+//  Created by Lawrence Dawson on 10/11/2016.
+//  Copyright © 2016 Lawrence Dawson. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellBanner: UITableViewCell {
+
+
+    @IBOutlet weak var mainImageView: UIImageView!
+    
+}
