75a539e3171c23aa4ae1c8e003fa308ed235e668
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-11-10 18:39:45 +0000

    made code changes for custom cell, not working

--- splittable_tt/splittable_tt/ViewController.swift
+++ splittable_tt/splittable_tt/ViewController.swift
@@ -9,61 +9,71 @@
 import UIKit
 import SwiftyJSON
 
-class ViewController: UITableViewController{
-   
+//let cell : Int!
+//let text : String!
+//let image : UIImage!
+
+class TableViewController: UITableViewController{
+ 
     var items = [ProfessionObject]()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        addData()
-    }
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            addData()
+        }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        let frame:CGRect = CGRect(x: 0, y: 100, width: self.view.frame.width, height: self.view.frame.height-100)
-//        self.tableView = UITableView(frame: frame)
-//        self.tableView.dataSource = self
-//        self.tableView.delegate = self
-//        self.view.addSubview(self.tableView)
-//        addData()
-//    }
-    func addData() {
-        print("add data called")
-        RestApiManager.sharedInstance.getSheetsuApi { (json: JSON) in
-            if let results = json.array {
-                for entry in results {
-                    self.items.append(ProfessionObject(json: entry))
+//            override func viewWillAppear(_ animated: Bool) {
+//                let frame:CGRect = CGRect(x: 0, y: 100, width: self.view.frame.width, height: self.view.frame.height-100)
+//                self.tableView = UITableView(frame: frame)
+//                self.tableView.dataSource = self
+//                self.tableView.delegate = self
+//                self.view.addSubview(self.tableView)
+//                addData()
+//            }
+        func addData() {
+            print("add data called")
+            RestApiManager.sharedInstance.getSheetsuApi { (json: JSON) in
+                if let results = json.array {
+                    for entry in results {
+                        self.items.append(ProfessionObject(json: entry))
+                    }
+                    self.items.sort{$0.sortOrder < $1.sortOrder}
+                    DispatchQueue.main.async(execute: {
+                        self.tableView.reloadData()
+                    })
                 }
-                self.items.sort{$0.sortOrder < $1.sortOrder}
-                DispatchQueue.main.async(execute: {
-                    self.tableView.reloadData()
-                })
             }
         }
-    }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items.count;
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "CELL")
-    
-        if cell == nil {
-            cell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: "CELL")
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return self.items.count;
         }
-        let profession = self.items[indexPath.row]
+    
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            //var cell = tableView.dequeueReusableCell(withIdentifier: "CELL")
+    
+//            if cell == nil {
+//                cell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: "CELL")
+//            }
+            let cell = Bundle.main.loadNibNamed("TableViewCell", owner: self, options: nil)?.first as! TableViewCell
+            
+            let profession = self.items[indexPath.row]
+    
+                if let url = NSURL(string: profession.pictureURL) {
+                    if let data = NSData(contentsOf: url as URL) {
         
-        if let url = NSURL(string: profession.pictureURL) {
-            if let data = NSData(contentsOf: url as URL) {
-                
-            cell?.imageView?.image = UIImage(data: data as Data)
-            }
+                         cell.mainImageView.image = UIImage(data: data as Data)
+                    }
+                }
+            cell.mainImageLabel.text = profession.name
+            return cell
         }
-        cell!.textLabel?.text = profession.name
-        return cell!
-    }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 250
+        }
+    
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+        }
     }
-}
